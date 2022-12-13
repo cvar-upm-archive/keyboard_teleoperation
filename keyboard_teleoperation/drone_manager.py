435763f5b1cbd83f954f0a33c87cb832f06541f8
Javilinos <javilinos@pop-os.localdomain>
2022-12-13 16:32:22 +0100

    working with new drone interface

--- keyboard_teleoperation/drone_manager.py
+++ keyboard_teleoperation/drone_manager.py
@@ -1,210 +1,219 @@
-from python_interface.drone_interface import DroneInterface
-from config_values import KeyMappings
 import threading
-from enum import Enum
+from python_interface.drone_interface_teleop import DroneInterfaceTeleop as DroneInterface
+from keyboard_teleoperation.config_values import KeyMappings
+
 
 class DroneManager:
-    def __init__(self, uav_list: list[DroneInterface], drone_id_list, pose_frame_id, twist_frame_id):
+    def __init__(self, uav_list: list[DroneInterface],
+                 drone_id_list, pose_frame_id, twist_frame_id):
         self.uav_list = uav_list
         self.drone_id_list = drone_id_list
         self.pose_frame_id = pose_frame_id
         self.twist_frame_id = twist_frame_id
+        self.value_common_behavior = {KeyMappings.TAKE_OFF_KEY.value: self.take_off,
+                                      KeyMappings.LAND_KEY.value: self.land,
+                                      KeyMappings.HOVER_KEY.value: self.hover,
+                                      KeyMappings.EMERGENCY_KEY.value: self.emergency_stop}
 
     def manage_common_behaviors(self, key):
 
-        if (key == KeyMappings.TAKE_OFF_KEY.value):
-
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
-                    self.execute_function(self.take_off, (self.uav_list[index],))
-
-        elif (key == KeyMappings.LAND_KEY.value):
-
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
-                    self.execute_function(self.land, (self.uav_list[index],))
-
-        elif (key == KeyMappings.HOVER_KEY.value):
-
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
-                    self.execute_function(self.hover, (self.uav_list[index],))
-
-        elif key == KeyMappings.EMERGENCY_KEY.value:
-
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
-                    self.execute_function(self.emergency_stop, (self.uav_list[index],))
+        self.execute_common_behaviors(self.value_common_behavior[key])
 
     def manage_speed_behaviors(self, key, value_list):
 
-        if (key == KeyMappings.FORWARD_KEY.value):
-            
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+        if key == KeyMappings.FORWARD_KEY.value:
+
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     lineal = [value_list[0], 0.0, 0.0]
-                    self.execute_function(self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
+                    self.execute_function(
+                        self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
 
-        elif (key == KeyMappings.BACKWARD_KEY.value):
+        elif key == KeyMappings.BACKWARD_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
-
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     lineal = [-value_list[0], 0.0, 0.0]
-                    self.execute_function(self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
+                    self.execute_function(
+                        self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
 
-        elif (key == KeyMappings.RIGHT_KEY.value):
+        elif key == KeyMappings.RIGHT_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
 
                     lineal = [0.0, value_list[0], 0.0]
-                    self.execute_function(self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
+                    self.execute_function(
+                        self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
 
-        elif (key == KeyMappings.LEFT_KEY.value):
+        elif key == KeyMappings.LEFT_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
 
                     lineal = [0.0, -value_list[0], 0.0]
-                    self.execute_function(self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
+                    self.execute_function(
+                        self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
 
-        elif (key == KeyMappings.UP_KEY.value):
+        elif key == KeyMappings.UP_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
 
                     lineal = [0.0, 0.0, value_list[1]]
-                    self.execute_function(self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
+                    self.execute_function(
+                        self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
 
-        elif (key == KeyMappings.DOWN_KEY.value):
+        elif key == KeyMappings.DOWN_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
 
                     lineal = [0.0, 0.0, -value_list[1]]
-                    self.execute_function(self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
+                    self.execute_function(
+                        self.move_at_speed, (self.uav_list[index], lineal, 0.0,))
 
-        elif (key == KeyMappings.ROTATE_RIGHT_KEY.value):
+        elif key == KeyMappings.ROTATE_RIGHT_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
-
-                    lineal = [0.0, 0.0, 0.0]
-                    self.execute_function(self.move_at_speed, (self.uav_list[index], lineal, value_list[2],))
-
-        elif (key == KeyMappings.ROTATE_LEFT_KEY.value):
-
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
 
                     lineal = [0.0, 0.0, 0.0]
-                    self.execute_function(self.move_at_speed, (self.uav_list[index], lineal, -value_list[2],))
+                    self.execute_function(
+                        self.move_at_speed, (self.uav_list[index], lineal, value_list[2],))
+
+        elif key == KeyMappings.ROTATE_LEFT_KEY.value:
+
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
+
+                    lineal = [0.0, 0.0, 0.0]
+                    self.execute_function(
+                        self.move_at_speed, (self.uav_list[index], lineal, -value_list[2],))
 
     def manage_pose_behaviors(self, key, value_list):
 
-        if (key == KeyMappings.FORWARD_KEY.value):
+        if key == KeyMappings.FORWARD_KEY.value:
 
             for index, drone_id in enumerate(self.drone_id_list):
-                if drone_id[1] == True:
+                if drone_id[1]:
 
                     position = [self.uav_list[index].position[0] + value_list[3],
                                 self.uav_list[index].position[1], self.uav_list[index].position[2]]
-                    
-                    self.execute_function(self.go_to_pose, (self.uav_list[index], position, self.uav_list[index].orientation[2],))
 
-        elif (key == KeyMappings.BACKWARD_KEY.value):
+                    self.execute_function(
+                        self.go_to_pose, (self.uav_list[index], position,
+                                          self.uav_list[index].orientation[2],))
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+        elif key == KeyMappings.BACKWARD_KEY.value:
+
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     position = [self.uav_list[index].position[0] - value_list[3],
                                 self.uav_list[index].position[1], self.uav_list[index].position[2]]
-            
-                    self.execute_function(self.go_to_pose, (self.uav_list[index], position, self.uav_list[index].orientation[2],))
 
-        elif (key == KeyMappings.RIGHT_KEY.value):
+                    self.execute_function(
+                        self.go_to_pose, (self.uav_list[index], position,
+                                          self.uav_list[index].orientation[2],))
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+        elif key == KeyMappings.RIGHT_KEY.value:
+
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     position = [self.uav_list[index].position[0], self.uav_list[index].position[1] +
                                 value_list[3], self.uav_list[index].position[2]]
-                    self.execute_function(self.go_to_pose, (self.uav_list[index], position, self.uav_list[index].orientation[2],))
+                    self.execute_function(
+                        self.go_to_pose, (self.uav_list[index], position,
+                                          self.uav_list[index].orientation[2],))
 
-        elif (key == KeyMappings.LEFT_KEY.value):
+        elif key == KeyMappings.LEFT_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     position = [self.uav_list[index].position[0], self.uav_list[index].position[1] -
                                 value_list[3], self.uav_list[index].position[2]]
-                    self.execute_function(self.go_to_pose, (self.uav_list[index], position, self.uav_list[index].orientation[2],))
+                    self.execute_function(
+                        self.go_to_pose, (self.uav_list[index], position,
+                                          self.uav_list[index].orientation[2],))
 
-        elif (key == KeyMappings.UP_KEY.value):
+        elif key == KeyMappings.UP_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     position = [self.uav_list[index].position[0], self.uav_list[index].position[1],
                                 self.uav_list[index].position[2] + value_list[4]]
-                    self.execute_function(self.go_to_pose, (self.uav_list[index], position, self.uav_list[index].orientation[2],))
+                    self.execute_function(
+                        self.go_to_pose, (self.uav_list[index], position,
+                                          self.uav_list[index].orientation[2],))
 
-        elif (key == KeyMappings.DOWN_KEY.value):
+        elif key == KeyMappings.DOWN_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     position = [self.uav_list[index].position[0], self.uav_list[index].position[1],
                                 self.uav_list[index].position[2] - value_list[4]]
-                    self.execute_function(self.go_to_pose, (self.uav_list[index], position, self.uav_list[index].orientation[2],))
+                    self.execute_function(
+                        self.go_to_pose, (self.uav_list[index], position,
+                                          self.uav_list[index].orientation[2],))
 
-        elif (key == KeyMappings.ROTATE_RIGHT_KEY.value):
+        elif key == KeyMappings.ROTATE_RIGHT_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     position = [self.uav_list[index].position[0],
                                 self.uav_list[index].position[1], self.uav_list[index].position[2]]
                     euler = self.uav_list[index].orientation
                     yaw = euler[2] + value_list[5]
 
-                    self.execute_function(self.go_to_pose, (self.uav_list[index], position, yaw,))
+                    self.execute_function(
+                        self.go_to_pose, (self.uav_list[index], position, yaw,))
 
-        elif (key == KeyMappings.ROTATE_LEFT_KEY.value):
+        elif key == KeyMappings.ROTATE_LEFT_KEY.value:
 
-            for index, drone_id in enumerate(self.drone_id_list): 
-                if drone_id[1] == True:
+            for index, drone_id in enumerate(self.drone_id_list):
+                if drone_id[1]:
                     position = [self.uav_list[index].position[0],
                                 self.uav_list[index].position[1], self.uav_list[index].position[2]]
                     euler = self.uav_list[index].orientation
                     yaw = euler[2] - value_list[5]
 
-                    self.execute_function(self.go_to_pose, (self.uav_list[index], position, yaw,))
+                    self.execute_function(
+                        self.go_to_pose, (self.uav_list[index], position, yaw,))
+
+    def execute_common_behaviors(self, method):
+        for index, drone_id in enumerate(self.drone_id_list):
+            if drone_id[1]:
+                self.execute_function(method, (self.uav_list[index],))
 
     def execute_function(self, target, args):
         try:
             threading.Thread(target=target, args=args, daemon=True).start()
         except Exception as e:
-            print('Error starting work thread.')
+            print('Error starting work thread: ', e)
 
     # FUNCTIONS TO CALL THE DRONE INTERFACES FUNCTIONS
 
-    def shutdown(self):
-        self.t.join()
+    # def shutdown(self):
+        # self.t.join()
 
     def take_off(self, uav: DroneInterface):
         uav.arm()
         uav.offboard()
         uav.takeoff(1.0, 1.0)
-        
+
     def land(self, uav: DroneInterface):
         uav.land(0.5)
 
     def hover(self, uav: DroneInterface):
-        uav.send_hover()
+        uav.motion_ref_handler.hover()
 
     def move_at_speed(self, uav: DroneInterface, lineal, yaw_speed):
-        uav.speed_motion_handler.send_speed_command_with_yaw_speed(
+        uav.motion_ref_handler.speed.send_speed_command_with_yaw_speed(
             lineal, self.twist_frame_id, yaw_speed)
 
     def go_to_pose(self, uav: DroneInterface, position, orientation):
-        uav.position_motion_handler.send_position_command_with_yaw_angle(
+        uav.motion_ref_handler.position.send_position_command_with_yaw_angle(
             position, None, self.pose_frame_id, self.twist_frame_id, orientation)
-        
+
     def emergency_stop(self, uav: DroneInterface):
         uav.send_emergency_killswitch_to_aircraft()
--- keyboard_teleoperation/keyboard_teleoperation.py
+++ keyboard_teleoperation/keyboard_teleoperation.py
@@ -1,19 +1,17 @@
 """Keyboard Teleoperation"""
-
+import threading
 import sys
 import rclpy
-import threading
-from enum import Enum
 
 import PySimpleGUI as sg
-from python_interface.drone_interface import DroneInterface
-import motion_reference_handlers.utils as mh_utils
-from main_window import MainWindow
-from localization_window import LocalizationWindow
-from settings_window import SettingsWindow
-from drone_manager import DroneManager
-from config_values import ControlValues
-from config_values import ControlModes
+from python_interface.drone_interface_teleop import DroneInterfaceTeleop as DroneInterface
+from keyboard_teleoperation.main_window import MainWindow
+from keyboard_teleoperation.localization_window import LocalizationWindow
+from keyboard_teleoperation.settings_window import SettingsWindow
+from keyboard_teleoperation.drone_manager import DroneManager
+from keyboard_teleoperation.config_values import ControlValues
+from keyboard_teleoperation.config_values import ControlModes
+
 
 def main():
     """entrypoint"""
@@ -25,12 +23,15 @@ def main():
     rclpy.init()
     if ',' in drone_id:
         drone_id_list = drone_id.split(",")
-        for id in drone_id_list:  uav_list.append(DroneInterface(id, verbose=is_verbose, use_sim_time=use_sim_time)) 
+        for uav_id in drone_id_list:
+            uav_list.append(DroneInterface(
+                uav_id, verbose=is_verbose, use_sim_time=use_sim_time))
     else:
-        uav_list.append(DroneInterface(drone_id, verbose=is_verbose, use_sim_time=use_sim_time))
+        uav_list.append(DroneInterface(
+            drone_id, verbose=is_verbose, use_sim_time=use_sim_time))
 
-    kt = KeyboardTeleoperation(uav_list, False)
-    while kt.execute_main_window(kt.main_window):
+    k_t = KeyboardTeleoperation(uav_list, False)
+    while k_t.execute_main_window(k_t.main_window):
         pass
 
     rclpy.shutdown()
@@ -42,40 +43,53 @@ class KeyboardTeleoperation:
     def __init__(self, list_drone_interface: list[DroneInterface], thread=False):
         self.uav_list = list_drone_interface
         drone_id_list = list()
-        
-        for uav in self.uav_list: drone_id_list.append([uav.get_namespace(), True])
-        
-        value_list = [ControlValues.SPEED_VALUE.value, ControlValues.VERTICAL_VALUE.value, ControlValues.TURN_SPEED_VALUE.value, ControlValues.POSITION_VALUE.value, ControlValues.ALTITUDE_VALUE.value,
-         ControlValues.TURN_ANGLE_VALUE.value, ControlValues.PITCH_ANGLE_VALUE.value, ControlValues.ROLL_ANGLE_VALUE.value, ControlValues.ATTITUDE_DURATION.value] 
+
+        for uav in self.uav_list:
+            drone_id_list.append([uav.get_namespace(), True])
+
+        value_list = [ControlValues.SPEED_VALUE.value, ControlValues.VERTICAL_VALUE.value,
+                      ControlValues.TURN_SPEED_VALUE.value, ControlValues.POSITION_VALUE.value,
+                      ControlValues.ALTITUDE_VALUE.value, ControlValues.TURN_ANGLE_VALUE.value,
+                      ControlValues.PITCH_ANGLE_VALUE.value,
+                      ControlValues.ROLL_ANGLE_VALUE.value,
+                      ControlValues.ATTITUDE_DURATION.value]
 
         self.localization_opened = False
-        
+
         sg.theme("DarkBlack1")
 
-        self.drone_manager = DroneManager(uav_list=self.uav_list, drone_id_list=drone_id_list, pose_frame_id='earth', twist_frame_id='earth')
+        self.drone_manager = DroneManager(
+            uav_list=self.uav_list, drone_id_list=drone_id_list,
+            pose_frame_id='earth', twist_frame_id='earth')
 
-        self.settings_window = SettingsWindow(font=("Terminus Font", 14), menu_font=("Ubuntu Mono", 18, 'bold'), value_list=value_list, title="Settings", enable_close_attempted_event=True)
+        self.settings_window = SettingsWindow(font=("Terminus Font", 14), menu_font=(
+            "Ubuntu Mono", 18, 'bold'), value_list=value_list, title="Settings",
+            enable_close_attempted_event=True)
 
-        self.localization_window = LocalizationWindow(font=("Terminus Font", 11), menu_font=("Ubuntu Mono", 13, 'bold'), uav_list=self.uav_list, title="Localization", use_default_focus=False, enable_close_attempted_event=True, resizable=True)
+        self.localization_window = LocalizationWindow(font=("Terminus Font", 11), menu_font=(
+            "Ubuntu Mono", 13, 'bold'), uav_list=self.uav_list, title="Localization",
+            use_default_focus=False, enable_close_attempted_event=True, resizable=True)
 
-        self.main_window = MainWindow(settings_window = self.settings_window, localization_window = self.localization_window, font=("Terminus Font", 14), menu_font=("Ubuntu Mono", 18, 'bold'),
-         drone_id_list=drone_id_list, value_list=value_list, title="Keyboard Teleoperation", finalize=True, return_keyboard_events=True)
+        self.main_window = MainWindow(settings_window=self.settings_window, localization_window=self.localization_window,
+                                      font=("Terminus Font", 14), menu_font=("Ubuntu Mono", 18, 'bold'),
+                                      drone_id_list=drone_id_list, value_list=value_list, title="Keyboard Teleoperation",
+                                      finalize=True, return_keyboard_events=True)
 
         if thread:
-            self.t = threading.Thread(target=self.tick_main_window, daemon=True)
-            self.t.start()
+            self._t = threading.Thread(
+                target=self.tick_main_window, daemon=True)
+            self._t.start()
         else:
             self.main_window.make_main_window()
-            
 
     def tick_main_window(self):
-        self.window = self.main_window.make_main_window()
-        while self.execute_main_window(self.window): #type: ignore
+        while self.execute_main_window(self.main_window):  # type: ignore
             pass
 
     def execute_main_window(self, window: MainWindow):
         event, values = window.read(timeout=50)  # type: ignore
-        control_mode, key, value_list, opened = self.main_window.event_handler(event, values)
+        control_mode, key, value_list, opened = self.main_window.event_handler(
+            event, values)
 
         if (control_mode is not None):
 
@@ -88,6 +102,7 @@ class KeyboardTeleoperation:
                 self.drone_manager.manage_pose_behaviors(key, value_list)
 
         return opened
+
 
 if __name__ == '__main__':
     main()
